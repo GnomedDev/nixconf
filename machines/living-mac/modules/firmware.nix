4c00deb47c3054c49e83687c984965fc62206928
GnomedDev <daisy2005thomas@gmail.com>
2025-12-21 22:21:33 +0000

    [living-mac] Add battery_charge_limit to firmware.nix

--- machines/living-mac/modules/firmware.nix
+++ machines/living-mac/modules/firmware.nix
@@ -1,8 +1,26 @@
 { ... }:
 {
+  # Enable T2 specific kernel drivers/wifi firmware.
   hardware.apple-t2.firmware.enable = true;
+
+  # Use the T2 patches to set the battery limit to 80% via systemd.
+  systemd.tmpfiles.settings = {
+    "t2linux-battery-limit" = {
+      "/sys/bus/acpi/drivers/applesmc/APP0001:00/battery_charge_limit" = {
+        "f+" = {
+          argument = "80";
+          group = "root";
+          user = "root";
+          mode = "0644";
+        };
+      };
+    };
+  };
+
+  # Enable power management via upower.
   services.upower.enable = true;
 
+  # Setup networking.
   networking.hostName = "living-mac";
   networking.networkmanager.enable = true;
 }
