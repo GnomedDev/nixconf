48834c3150ebe0a3eb0c56a5f12a287aff223bff
GnomedDev <daisy2005thomas@gmail.com>
2026-04-04 12:22:56 +0100

    Bump nixpkgs and azahar

--- common/packages/azahar-bin.nix
+++ common/packages/azahar-bin.nix
@@ -2,19 +2,20 @@
 let
   pname = "azahar";
   version = pkgs.azahar.version;
+  srcName = "azahar-macos-arm64-${version}";
 in
 pkgs.stdenvNoCC.mkDerivation {
   inherit pname version;
 
   src = pkgs.fetchurl {
-    url = "https://github.com/azahar-emu/azahar/releases/download/${version}/azahar-${version}-macos-arm64.zip";
-    sha256 = "1fc823cdcae00c9f3aeda7c19c1a990b2bebd0f9e03328d3f74b38b6c4cb23f7";
+    url = "https://github.com/azahar-emu/azahar/releases/download/${version}/${srcName}.zip";
+    sha256 = "sha256-REJHvRTUVmALjbwVxNQSLmvQ5wj1g5eZ322PypCaa4I=";
   };
 
   unpackPhase = "${lib.getExe pkgs.unzip} $src";
 
   installPhase = ''
     mkdir -p $out/Applications
-    cp -r azahar-${version}-macos-arm64/Azahar.app $out/Applications
+    cp -r ${srcName}/Azahar.app $out/Applications
   '';
 }
