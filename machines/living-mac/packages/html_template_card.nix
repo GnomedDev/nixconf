a8376417884471980da26d6206b7f15d8a343345
GnomedDev <daisy2005thomas@gmail.com>
2026-05-15 15:52:09 +0100

    [living-mac] Fix html_template_card filename passthrough

--- machines/living-mac/packages/html_template_card.nix
+++ machines/living-mac/packages/html_template_card.nix
@@ -2,13 +2,17 @@
   stdenvNoCC,
   jninja2TemplateSrc,
 }:
+let
+  filename = "html-template-card.js";
+in
 stdenvNoCC.mkDerivation {
   pname = "Home-Assistant-Lovelace-HTML-Jinja2-Template-card";
   version = jninja2TemplateSrc.shortRev;
   src = jninja2TemplateSrc;
 
+  passthru.entrypoint = filename;
   installPhase = ''
     mkdir $out
-    cp dist/html-template-card.js $out
+    cp dist/${filename} $out
   '';
 }
